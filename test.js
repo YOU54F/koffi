d51124e8d3d97964cfc1a54f303dc0dd2c717c1f
Yousaf Nabi <you@saf.dev>
2023-04-22 14:47:16 +0100

    ci: macos use nvs to switch archs

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const koffi = require('koffi');
 const path = require('path');
+console.log({ arch: process.arch, platform: process.platform });
 const lib = koffi.load(
   path.join(
     'ffi',
     `${process.platform}-${process.arch}`,
-    `${process.platform !== 'win32' ? "lib" : ''}pact_ffi.${
+    `${process.platform !== 'win32' ? 'lib' : ''}pact_ffi.${
       process.platform === 'win32'
         ? 'dll'
         : process.platform === 'darwin'
