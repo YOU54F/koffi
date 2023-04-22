a363f08ce22a7421ff8614c98a8fa2a948588c8a
Yousaf Nabi <you@saf.dev>
2023-04-22 18:27:17 +0100

    ci(win): koffi print path

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 console.log({ arch: process.arch, platform: process.platform });
 const koffi = require('koffi');
 const path = require('path');
-const lib = koffi.load(
-  path.join(
-    'ffi',
-    `${process.platform}-${process.arch}`,
-    `${process.platform !== 'win32' ? 'lib' : ''}pact_ffi.${
-      process.platform === 'win32'
-        ? 'dll'
-        : process.platform === 'darwin'
-        ? 'dylib'
-        : 'so'
-    }`
-  )
+const libPath = path.join(
+  'ffi',
+  `${process.platform}-${process.arch}`,
+  `${process.platform !== 'win32' ? 'lib' : ''}pact_ffi.${
+    process.platform === 'win32'
+      ? 'dll'
+      : process.platform === 'darwin'
+      ? 'dylib'
+      : 'so'
+  }`
 );
+console.log(libPath);
+const lib = koffi.load(libPath);
 const version = lib.func('const char *pactffi_version(void)');
 console.log(`Hello Ffi!\n ${version()}`);
